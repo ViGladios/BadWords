1a78696fb08f460f8b608001fd6aeff70805b3d2
MrDoni98 <danil.bartel@gmail.com>
2016-07-28 23:10:20 +0600

    Update Main.php

--- src/BadWords/Main.php
+++ src/BadWords/Main.php
@@ -4,24 +4,44 @@ namespace BadWords;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\utils\TextFormat as C;
+use pocketmine\utils\TextFormat as F;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\Player;
 
 class Main extends PluginBase implements Listener{
 
-    $worlds = array(
+    $words = array(
       'дибил',
       'сука',
       'хуй',
       'пидар',
-      'пизда'
+      'пизда',
+      'блять',
+      'бля',
+      'ебать'
+    );
+
+    $words2 = array(
+      'можно админку',
+      'дайте админку',
+      'дайте оп',
+      'дай оп',
+      'можно оп',
+      'можно креатив',
+      'дай креатив',
+      'дай креат',
+      'дай модерку',
+      'можно быть модератором'
     );
 
     public function onChat(PlayerChatEvent $ev){
-       if(strpos($worlds, strtolower($ev->getMessage()))){
-          $event->getName()->sendMessage(C::RED."You cannot say that!");
+       if(strpos($words, strtolower($ev->getMessage()))){
+          $ev->getPlayer()->sendMessage(F::RED."Нецензурная брань!");
   	      $ev->setCancelled();
+       }
+       if(strpos($words2, strtolower($ev->getMessage()))){
+           $ev->getPlayer->kick(F::RED."Прекрати попрошайничать");
+           $ev-setCancelled();
        }
     }
 }
